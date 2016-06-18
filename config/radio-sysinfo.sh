d2d5076d692c414c8701789f2717d606ae2d2840
Chris P <sahib@online.de>
2016-06-19 00:15:54 +0200

    Add cpu-{temp,freq} to sysinfo

--- config/radio-sysinfo.sh
+++ config/radio-sysinfo.sh
@@ -5,8 +5,10 @@ MEM_USAGE=$(free | grep Mem | awk '{printf("%.0f\n", $3/$2 * 100.0)}')
 IP_ADDR=$(ip addr show wlan0 | grep -m 1 'inet' | awk  '{print $2}' | sed 's/...$//g')
 UPTIME_HOURS=$(uptime | awk '{print $1}')
 WIFI_SIGNAL=$(awk 'NR==3 {print $4}''' /proc/net/wireless)
+CPU_TEMP=$(cat /sys/class/thermal/thermal_zone0/temp | awk '{ print $0 / 1000 }')
+CPU_FREQ=$(cat /sys/devices/system/cpu/cpu0/cpufreq/cpuinfo_cur_freq | awk '{ print $0 / 1000 }')
 
-printf "π %3d%% ⏸ %3d%%\n" ${CPU_USAGE} ${MEM_USAGE}
-printf "IP: %15s\n" ${IP_ADDR}
-printf "❤ %s ৹C\n" ${UPTIME_HOURS}
-printf "ψ %3.1f%%\n"  ${WIFI_SIGNAL}
+printf "CPU: %3d%%  MEM: %3d%%\n" ${CPU_USAGE} ${MEM_USAGE}
+printf "ADDR: %14s\n" ${IP_ADDR}
+printf "❤ %sH  %4sMHz\n" ${UPTIME_HOURS} ${CPU_FREQ}
+printf "ψ %3.3f%%   %3.3f৹C\n"  ${WIFI_SIGNAL} ${CPU_TEMP}
